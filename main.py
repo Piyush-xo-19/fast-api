111813225a79c8c9ec5a21bf13b8ca5f3e6fda59
Piyush Gupta <pg7267330@gmail.com>
2025-11-25 15:34:11 +0530

    created and end point to view individual data

--- main.py
+++ main.py
@@ -18,3 +18,11 @@ def about():
 def view():
     
     return load_data()
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id:str):
+    data=load_data()
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        return{"message":"patient id not found enter a valid patient id "}
